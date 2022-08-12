b048347e4a9521f0c8bbe62d7ed64caeee5f4d19
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-08-12 00:03:56 -0500

    added paypal libraries

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/globals.css'
+import Navigation from '../components/navigation/Navigation'
+
+
+import { PayPalScriptProvider, PayPalButtons,usePayPalScriptReducer } from "@paypal/react-paypal-js";
+const initialOptions = {
+  "client-id": "test",
+  currency: "USD",
+  intent: "capture",
+  "data-client-token": "abc123xyz==",
+};
+
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [{ isPending }] = usePayPalScriptReducer();
+  return( <>
+    <Navigation/>
+  
+  <PayPalScriptProvider deferLoading={true} options={initialOptions}>
+            <Component {...pageProps} />
+            {isPending?<p>Loading...</p>:<PayPalButtons style={{ layout: "horizontal" }} />}
+  </PayPalScriptProvider>
+  </>
+  )
 }
 
 export default MyApp
---
+++ pages/_documents.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+    <script src="https://www.paypalobjects.com/api/checkout.js" />
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
